810a53638aed6043454eb6cc5694c77f031f013f
Torbjørn Øvrebekk <torbjorn.ovrebekk@nordicsemi.no>
2018-09-12 16:50:49 +0200

    minor graphical optimizations

--- app_display.c
+++ app_display.c
@@ -3,6 +3,7 @@
 #include "gfx_glue_layer.h"
 #include "nrf_gfx.h"
 #include "nrf_lcd.h"
+#include "font.h"
 
 static nrf_lcd_t m_led_matrix = GFX_LED_DRV_MATRIX;
 
@@ -18,31 +19,48 @@ void app_display_init(void)
     m_led_matrix.p_lcd_cb = &lcd_cb;
     err_code = nrf_gfx_init(&m_led_matrix);
     APP_ERROR_CHECK(err_code);
+    
+        nrf_gfx_point_t point = {.x = 10, .y = 10};
+    err_code = nrf_gfx_print(&m_led_matrix, &point, CL_GREEN, "Hei", &orkney_8ptFontInfo, false);
 }
 
 void app_display_draw_paddles(uint32_t p1_pos, uint32_t p2_pos, uint32_t p1_color, uint32_t p2_color)
 {
-    if(p1_pos < (PADDLE_HEIGHT / 2)) p1_pos = PADDLE_HEIGHT / 2;
-    else if(p1_pos > (32 - (PADDLE_HEIGHT / 2))) p1_pos = 32 - (PADDLE_HEIGHT / 2);
-    if(p2_pos < (PADDLE_HEIGHT / 2)) p2_pos = PADDLE_HEIGHT / 2;
-    else if(p2_pos > (32 - (PADDLE_HEIGHT / 2))) p2_pos = 32 - (PADDLE_HEIGHT / 2);
-
-    nrf_gfx_rect_t rect = {.x = 0, .y = 0, .width = 2, .height = 32};
-    nrf_gfx_rect_draw(&m_led_matrix, &rect, 1, CL_BLACK, true);
-    nrf_gfx_rect_t rect2 = {.x = 0, .y = p1_pos - (PADDLE_HEIGHT / 2), .width = 2, .height = PADDLE_HEIGHT};
-    nrf_gfx_rect_draw(&m_led_matrix, &rect2, 1, p1_color, true);
+    static uint32_t p1_pos_last = 0xFFFF;
+    static uint32_t p1_col_last = 0;
+    static uint32_t p2_pos_last = 0xFFFF;
+    static uint32_t p2_col_last = 0;
     
-    nrf_gfx_rect_t rect3 = {.x = 62, .y = 0, .width = 2, .height = 32};
-    nrf_gfx_rect_draw(&m_led_matrix, &rect3, 1, CL_BLACK, true);
-    nrf_gfx_rect_t rect4 = {.x = 62, .y = p2_pos - (PADDLE_HEIGHT / 2), .width = 2, .height = PADDLE_HEIGHT};
-    nrf_gfx_rect_draw(&m_led_matrix, &rect4, 1, p2_color, true);
+    if(p1_pos != p1_pos_last || p1_color != p1_col_last)
+    {
+        if(p1_pos < (PADDLE_HEIGHT / 2)) p1_pos = PADDLE_HEIGHT / 2;
+        else if(p1_pos > (32 - (PADDLE_HEIGHT / 2))) p1_pos = 32 - (PADDLE_HEIGHT / 2);
+        nrf_gfx_rect_t rect = {.x = 0, .y = 0, .width = 2, .height = 32};
+        nrf_gfx_rect_draw(&m_led_matrix, &rect, 1, CL_BLACK, true);
+        nrf_gfx_rect_t rect2 = {.x = 0, .y = p1_pos - (PADDLE_HEIGHT / 2), .width = 2, .height = PADDLE_HEIGHT};
+        nrf_gfx_rect_draw(&m_led_matrix, &rect2, 1, p1_color, true);
+        p1_pos_last = p1_pos;
+        p1_col_last = p1_color;
+    }
+    
+    if(p2_pos != p2_pos_last || p2_color != p2_col_last)
+    {
+        if(p2_pos < (PADDLE_HEIGHT / 2)) p2_pos = PADDLE_HEIGHT / 2;
+        else if(p2_pos > (32 - (PADDLE_HEIGHT / 2))) p2_pos = 32 - (PADDLE_HEIGHT / 2);    
+        nrf_gfx_rect_t rect3 = {.x = 62, .y = 0, .width = 2, .height = 32};
+        nrf_gfx_rect_draw(&m_led_matrix, &rect3, 1, CL_BLACK, true);
+        nrf_gfx_rect_t rect4 = {.x = 62, .y = p2_pos - (PADDLE_HEIGHT / 2), .width = 2, .height = PADDLE_HEIGHT};
+        nrf_gfx_rect_draw(&m_led_matrix, &rect4, 1, p2_color, true);
+        p2_pos_last = p2_pos;
+        p2_col_last = p2_color;
+    }
 }
 
 void app_display_draw_ball(uint32_t pos_x, uint32_t pos_y)
 {
     static uint32_t previous_pos_x = 1, previous_pos_y = 1;
     static nrf_gfx_rect_t rect_ball_prev = {.x = 1, .y = 1, .width = 2, .height = 2};
-    nrf_gfx_rect_t rect_ball =  {.x = pos_x, .y = pos_y, .width = 2, .height = 2};
+    nrf_gfx_rect_t rect_ball =  {.x = pos_x + 2, .y = pos_y, .width = 2, .height = 2};
     nrf_gfx_rect_draw(&m_led_matrix, &rect_ball_prev, 1, CL_BLACK, true);
     nrf_gfx_rect_draw(&m_led_matrix, &rect_ball, 1, CL_WHITE, true);
     
